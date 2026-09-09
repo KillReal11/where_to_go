6d87a7f3cb592647ce4a4a42b6d9a9763a141e48
KillReal11 <dawiduk2013@yandex.ru>
2026-09-09 20:15:09 +0500

    Added places model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-print("Create your models here.")
+print("Запущен файл models.py")
+
+
+class Places(models.Model):
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Краткое описание', blank=True)
+    description_long = models.TextField('Полное описание', blank=True)
+    latitude = models.FloatField('Широта')
+    longitude = models.FloatField('Долгота')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'место'
+        verbose_name_plural = 'места'
