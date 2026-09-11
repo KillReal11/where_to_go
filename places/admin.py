2bc44ebcbf217f61a584f1f36a6f108564cc0da2
KillReal11 <dawiduk2013@yandex.ru>
2026-09-11 21:48:27 +0500

    added image preview

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from places.models import Place, Image
+from django.utils.html import format_html
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    readonly_fields = ['preview']
+    fields = ["file", "preview", "position_number"]
+
+    def preview(self, place):
+        return format_html(
+            '<img src="{url}" style="max-height: 100px; margin: 5px;"/>',
+            url=place.file.url,
+        )
+
 
 
 @admin.register(Place)
