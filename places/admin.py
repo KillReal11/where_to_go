98bab65dd3626398af507992a3b4a9b59089c770
KillReal11 <dawiduk2013@yandex.ru>
2026-09-11 20:58:11 +0500

    added option to upload images on place page in admin

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from places.models import Place, Image
 
-# Register your models here.
-admin.site.register(Place)
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+
+
 admin.site.register(Image)
