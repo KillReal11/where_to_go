5c9714d0ed002afde2f5cd7520b70a39ca23d8fb
KillReal11 <dawiduk2013@yandex.ru>
2026-09-16 20:11:21 +0500

    shrink code

--- where_to_go/management/commands/load_place.py
+++ where_to_go/management/commands/load_place.py
@@ -44,12 +44,12 @@ def load_place(place_json_url):
     for position_number, image_url in enumerate(image_urls):
         response_image = requests.get(image_url)
         response_image.raise_for_status()
-        image = Image(place=place, position_number=position_number)
-        image_filename = os.path.basename(urlsplit(image_url).path)
-
-        image.file.save(
-            image_filename,
-            ContentFile(response_image.content),
-            save=True
+        Image.objects.create(
+            place=place,
+            position_number=position_number,
+            file=ContentFile(
+                response_image.content,
+                name=os.path.basename(urlsplit(image_url).path),
+            )
         )
     return place
